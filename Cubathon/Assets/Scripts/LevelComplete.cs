5235d8037999949bd9c530363511557f037d3223
ilian.stefanov <ilian.stefanov@primeholding.com>
2019-11-06 10:29:34 +0200

    Uploading Cubathon game

---
+++ Cubathon/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+
+    public int maxLevel;
+
+    public void LoadNextLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
